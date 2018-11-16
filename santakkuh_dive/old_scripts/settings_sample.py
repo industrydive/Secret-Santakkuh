b0947207e14a07db8ed5f324f333da55a8731e3e
Bethany Morin <bmorin@industrydive.com>
2018-11-16 10:08:24 -0500

    Finalized updated send_emails code and sample settings

--- santakkuh_dive/old_scripts/settings_sample.py
+++ santakkuh_dive/old_scripts/settings_sample.py
@@ -1,6 +1,7 @@
-SQLITE_FILENAME = 'santakkuh_dive.db'
-YEAR = 2017
+SQLITE_FILENAME = 'db/santakkuh_dive.db'
+YEAR = 1999
 SOURCE_FILE = 'tests/avengers.csv'
 OUT_FILE = 'csv/assignments.csv'
 FROM_EMAIL = ''
 EMAIL_PW = ''
+TEST_EMAILS = []
